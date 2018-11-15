8b0351ac48dbf0e67d7d7880da08bd72ab1b3bcb
Momo_Ibn_Fatou <43034190+MomoNdour@users.noreply.github.com>
2018-11-15 15:39:28 +0000

    first commit momo

--- index.php
+++ index.php
@@ -1,34 +1,44 @@
 <?php
-include('Controler/Connexion/Connexion.php');
-$db = Connexion();
+    require 'View/vendor/autoload.php';
+
+    //Configuration twig
+$loader = new Twig_Loader_Filesystem('View');
+
+$twig = new Twig_Environment($loader, [
+    'cache' => false,
+]);
+
+$link = basename($_SERVER['REQUEST_URI']); //Pour n'afficher que le dernier repertoire de la barre d'adresse
+
+// if($link == 'accueil')
+// {
+//     echo $twig->render('pages/accueil.twig',['title' => $link]);
+// }else if($link == 'about')
+// {
+//     echo $twig->render('pages/about.twig',['title' => $link]);
+// }else if($link == 'politique')
+// {
+//     echo $twig->render('pages/politique.twig',['title' => $link]);
+// }
+// else{
+//     echo $twig->render('pages/404.twig',['title' => $link]);
+// }
+
+
+    switch($link)
+    {
+        case 'accueil':
+            echo $twig->render('pages/accueil.twig');
+            break;
+        case 'about':
+            echo $twig->render('pages/about.twig');
+            break;
+        case 'politique':
+            echo $twig->render('pages/politique.twig');
+            break;
+        default:
+            header('HTTP/2.0 404 Not found');
+            echo $twig->render('pages/404.twig');
+            break;
+    }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-    <form action="Modele/Requete.php" method="post">
-    <div>
-    <input type="email" name="Email_utilisateur">
-    </div>
-    <div>
-    <input type="email" name="Email_recepteur">
-    </div>
-    <div>
-    <textarea name="message_utilisateur" id="" cols="30" rows="10"></textarea>
-    </div>
-    <div>
-    <input type="file" name="nom_fichier">
-    </div>
-    <div>
-    <button type="submit" name="Envoyer">Envoyer</button>
-    </div>
-    </form>
-</body>
-</html>
